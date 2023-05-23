bdd977867ce2d258b979c3bc7789575d48df3f96
Kanta Oikawa <k.oikawa@kantacky.com>
2023-05-23 16:51:51 +0900

    [Add] Add SearchButton

---
+++ STRAY-IT/Sources/STRAYIT/SearchButton.swift
@@ -0,0 +1,14 @@
+import Resource
+import SwiftUI
+
+struct SearchButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchButton()
+    }
+}
